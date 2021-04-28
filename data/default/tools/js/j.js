bebc787fd968c72328966d3f7aa148d047a1c362
andrey <lamzin.an@gmail.com>
2021-04-28 21:29:11 +0300

    Add jexec and xdb-open for a#href support

--- data/default/tools/js/j.js
+++ data/default/tools/js/j.js
@@ -1,3 +1,4 @@
+// v 1.0.0
 //location this file relative djs.exe: "default/tools/js/j.js"
 window.QtBrige = {
 	/**
@@ -24,12 +25,15 @@ PHP.scandir = function(path) {
 	return b;
 }
 
-window.FILE_APPEND = 1;
+window.FILE_APPEND = 8;
 
 /** @class localize */
 window.addEventListener('load', __jqtCreateLocalizator, false);
 function __jqtCreateLocalizator()
 {
+	// Set xdg-open for all links
+	__jqtSetXdgOpenForLinks();
+	
 	var locale = document.getElementsByTagName('html')[0].getAttribute('lang'),
 		filePath = Qt.appDir() + '/js/ru.json', loc;
 	if (PHP.file_exists(filePath)) {
@@ -44,5 +48,107 @@ function __jqtCreateLocalizator()
 		//Локализуем все value в input[type=button] и весь label.innerHTML
 	} else {
 		//alert(filePath);
+	}
+}
+
+
+// class jexec
+function jExec() {}
+/**
+ * @description Run system command.
+ * @param {String} command (command for call)
+ * @param {Function|Object|Array} onFinish. Object: {context:Object, method:Function}. Array: [context:Object, method:Function]
+ * @param {Function|Object|Array} onStdout. See onFinish format
+ * @param {Function|Object|Array} onError. See onFinish format
+ * @return Array [sysId, procId] sysId - system process id in linux; procId - handle for get sysId with PHP.getSysId(procId);
+*/
+jExec.prototype.run = function(command, onFinish, onStdout, onStderr) {
+	var sOnFinishname = this.generateName('onFinish'),
+		sOnOutname = this.generateName('onStdout'),
+		sOnErrorname = this.generateName('onStderr'),
+		o = this;
+	
+	window[sOnFinishname] = function(stdout, stderr) {
+		o.apply(onFinish, [stdout, stderr]);
+	}
+	window[sOnOutname] = function(stdout) {
+		o.apply(onStdout, [stdout]);
+	}
+	window[sOnErrorname] = function(stderr) {
+		o.apply(onStderr, [stderr]);
+	}
+	
+	var procId = PHP.exec(command, sOnFinishname, sOnOutname, sOnErrorname),
+		sysId = parseInt(PHP.getSysId(procId));
+	sysId = isNaN(sysId) ? 0 : sysId;
+	
+	return [sysId, procId];
+}
+/**
+ * @param {Function|Object|Array} callable. Object: {context:Object, method:Function}. Array: [context:Object, method:Function]
+ * @param {Array} args
+*/
+jExec.prototype.apply = function(callable, args) {
+	var cback = callable,
+		w = window,
+		isF  = ( cback instanceof Function ),
+		ctx, f;
+
+	if(cback && isF) {
+		ctx = w;
+		f = cback;
+	}
+	  
+	if (
+		cback && cback.m instanceof Function
+		&& (cback.context instanceof Object)
+		){
+			ctx = cback.context;
+			f = cback.m;
+	}
+	  
+	if (cback && cback[0] instanceof Object &&  cback[1] instanceof Function) {
+		ctx = cback[0];
+		f = cback[1];
+	}
+	  
+	if (f instanceof Function){
+		return f.apply(ctx, args);
+	}
+	  
+	return null;
+}
+/**
+ * @param {String}
+*/
+jExec.prototype.generateName = function(prefix) {
+	var dt = new Date(), suffix = String(dt.getTime()) + String(Math.random()).replace('.', '_');
+	return prefix + suffix;
+}
+
+/**
+ * @see Jexec.run
+*/
+function jexec(command, onFinish, onStdout, onStderr) {
+	var o = new jExec();
+	return o.run(command, onFinish, onStdout, onStderr);
+}
+
+// end class jexec
+
+// Set xdg-open for all links
+
+function __jqtSetXdgOpenForLinks() {
+	var ls = document.getElementsByTagName('a'), i, lnk, ctx, Null = new Function();
+	for (i = 0; i < ls.length; i++) {
+		lnk = ls[i].getAttribute('href');
+		if (lnk.indexOf('http') === 0) {
+			ctx = ls[i];
+			ls[i].addEventListener('click', function(evt){
+				evt.preventDefault();
+				PHP.exec('xdg-open ' + lnk, Null, Null, Null);
+				return false;
+			}, false);
+		}
 	}
 }
