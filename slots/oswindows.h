d1364dfde4987bf6a4e66c31cb91b23528dd9dda
andrey <lamzin.an@gmail.com>
2022-03-18 18:02:02 +0300

    Win32 os.h os.cpp

---
+++ slots/oswindows.h
@@ -0,0 +1,26 @@
+#ifndef OS_H
+#define OS_H
+
+#include <QObject>
+#include <QWidget>
+#include <QString>
+#include <QByteArray>
+#include "../lib/utils.h"
+#include "windows.h"
+#include "../ui/cwebview.h"
+class OS : public QObject
+{
+    Q_OBJECT
+public:
+    explicit OS(QObject *parent = 0, CWebView *webView = 0);
+    QString getLocalFileStartUrl();
+signals:
+    
+public slots:
+  int ShellExecuteQ(QString operation, QString path, QString params, QString directory, bool showCmdLine = false);
+  QString getTempFolderPath();
+private:
+  CWebView *webView;
+};
+
+#endif // OS_H
