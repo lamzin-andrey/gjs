58c3cde2b26ac514e8376e7903e5db92f97c979b
andrey <lamzin.an@gmail.com>
2022-04-03 13:59:58 +0300

    Ещё немнрого французских булок и чаю

--- data/default/doc/j/src/simpledemo.js
+++ data/default/doc/j/src/simpledemo.js
@@ -19,6 +19,7 @@ var Demo = {
 		if (!PHP.file_exists('/usr')) {
 			cmd = 'notepad';
 		}
+		o.outputIndex = 0;
 		this.xtId = jexec(cmd, [o, o.onFinishXT], [o, o.onStdOutXT], [o, o.onStdErrXT]);
 		
 	},
@@ -28,13 +29,47 @@ var Demo = {
 		if (!PHP.file_exists('/usr')) {
 			cmd = 'TASKKILL /PID ' + this.xtId[0] + ' /T';
 		}
-		
+		o.outputIndex = 0;
 		this.xtId = jexec(cmd, [o, o.onFinishXT], [o, o.onStdOutXT], [o, o.onStdErrXT]);
 	},
 	
+	runXTerm2: function(){
+		var o = this,
+			cmd = 'xterm';
+		if (!PHP.file_exists('/usr')) {
+			cmd = 'notepad';
+		}
+		o.outputIndex = 2;
+		this.xtId = jexec(cmd, [o, o.onFinishXT], [o, o.onStdOutXT], [o, o.onStdErrXT]);
+		
+	},
+	closeXTerm2: function(){
+		var o = this, cmd = 'kill ' + this.xtId[0];
+		
+		if (!PHP.file_exists('/usr')) {
+			cmd = 'TASKKILL /PID ' + this.xtId[0] + ' /T';
+		}
+		o.outputIndex = 2;
+		this.xtId = jexec(cmd, [o, o.onFinishXT], [o, o.onStdOutXT], [o, o.onStdErrXT]);
+	},
+    checkXTerm2: function() {
+		var suffix = '2';
+		if (!this.xtId) {
+			e('xtStdErr' + suffix).innerHTML += '<div>Proc is not run yet.</div>';
+		}
+        if (PHP.isRun(this.xtId[1])) {
+			e('xtStdOut' + suffix).innerHTML += '<div>Proc is run.</div>';
+		} else {
+			e('xtStdOut' + suffix).innerHTML += '<div>Proc is not run.</div>';
+		}
+    },
 	onFinishXT: function(stdout, stderr) {
-		e('xtStdOut').innerHTML += '<div>' + stdout + '</div>';
-		e('xtStdErr').innerHTML += '<div>' + stderr + '</div>';
+		var suffix = '';
+		if (this.outputIndex == 2) {
+			suffix = '2'
+		}
+		e('xtStdOut' + suffix).innerHTML += '<div>' + stdout + '</div>';
+		e('xtStdErr' + suffix).innerHTML += '<div>' + stderr + '</div>';
 	},
 	
 	onStdOutXT: function(stdout) {
