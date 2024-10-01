b9da232448c80892a912c05c1c90caa1d56267ea
andrey <lamzin.an@rosfines.ru>
2024-10-01 16:57:30 +0300

    QDJS Doc locale

---
+++ data/default/doc/j/src/lang/local.js
@@ -0,0 +1,21 @@
+var langEn = {
+	'Choose a directory' : 'Choose a directory',
+	'Choose a file' : 'Choose a file',
+	'Size of file' : 'Size of file',
+	'Need choose a file' : 'Need choose a file',
+	'Need choose a text file' : 'Need choose a text file',
+	'Need choose a text file for save' : 'Need choose a text file for save',
+	'bytes' : 'bytes',
+	'Bytes writed:' : 'Bytes written:',
+	'hAutonullLegend' : 'Auto prefix \'?\' before types',
+	'bSelectDirectory' : 'Browse',
+	'hLangE' : 'English',
+	'hLangR' : 'Russian',
+	'bAddField' : 'Add field',
+	'bGenerate' : 'Generate',
+	'bRemove' : 'Remove',
+	'' : ''
+};
+
+var jaqedLang = langEn;
+
---
+++ data/default/doc/j/src/lang/localru.js
@@ -0,0 +1,23 @@
+var langRu = {
+	'Готово!' : 'Done!',
+	'Choose a directory' : 'Выберите каталог',
+	'Choose a file' : 'Выберите файл',
+	'Size of file' : 'Размер файла',
+	'Need choose a file' : 'Надо выбрать файл',
+	'Need choose a text file' : 'Надо сначала выбрать текстовый файл',
+	'Need choose a text file for save' : 'Надо выбрать файл для сохранения',
+	'bytes' : 'байт',
+	'Bytes writed:' : 'Записано байт:',
+	'hAutonullLegend' : 'Auto prefix \'?\' before types',
+	'bSelectDirectory' : 'Browse',
+	'hLangE' : 'English',
+	'hLangR' : 'Russian',
+	'bAddField' : 'Add field',
+	'bGenerate' : 'Generate',
+	'bRemove' : 'Remove',
+	'' : ''
+};
+
+var jaqedLang = langRu;
+
+
